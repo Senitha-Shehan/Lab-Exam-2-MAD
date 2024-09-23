4073232f163686ec7ffe627a0769ab3e84987dc9
Senitha-Shehan <senitha200109@gmail.com>
2024-09-23 09:01:26 +0530

    First Commit
    Commit

---
+++ app/src/main/java/com/example/mealmaster/onboringscreen.kt
@@ -0,0 +1,18 @@
+package com.example.mealmaster
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class onboringscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboringscreen)
+
+
+
+    }
+}
